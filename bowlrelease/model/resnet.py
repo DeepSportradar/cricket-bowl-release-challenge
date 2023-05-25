e135b63876f4ff88feb025e5c4324b7983f4c01b
Davide Zambrano <davide.zambrano@gmail.com>
2023-05-25 14:56:13 +0200

    solving issue with model

--- bowlrelease/model/resnet.py
+++ bowlrelease/model/resnet.py
@@ -1,4 +1,5 @@
 import logging
+import math
 
 import torch
 from torchvision.models import ResNet18_Weights, resnet18
@@ -6,33 +7,31 @@ from torchvision.models import ResNet18_Weights, resnet18
 LOGGER = logging.getLogger(__name__)
 
 
-class CricketBaseModel(object):
+class CricketBaseModel(torch.nn.Module):
     def __init__(
         self,
     ):
-        self.model = resnet18(weights=ResNet18_Weights.DEFAULT)
-        num_ftrs = self.model.fc.in_features
+        super().__init__()
+        self.rn_model = resnet18(weights=ResNet18_Weights.DEFAULT)
+        num_ftrs = self.rn_model.fc.in_features
 
-        # Initialize the Weight Transforms
-        self.weights = ResNet18_Weights.DEFAULT
-        self.preprocess = self.weights.transforms(antialias=True)
-
-        self.feat = torch.nn.Linear(num_ftrs, 512)
+        self.rn_model.fc = torch.nn.Linear(num_ftrs, 512)
         self.feat_bn = torch.nn.BatchNorm1d(512)
         self.classifier = torch.nn.Linear(512, 2)
 
+        torch.nn.init.kaiming_uniform_(self.rn_model.fc.weight)
+        torch.nn.init.kaiming_uniform_(self.classifier.weight)
+
     def forward(self, x):
-        x = self.model(x)
-        x = self.feat(x)
+        x = self.rn_model(x)
         x = self.feat_bn(x)
         x = self.classifier(x)
         return x
 
 
 def get_model(device, resume):
-    model_base = CricketBaseModel()
-    model = model_base.model.to(device)
+    model = CricketBaseModel()
     if resume:
         LOGGER.info(f"Loading model parameters from {resume}")
         model.load_state_dict(torch.load(resume))
-    return model
+    return model.to(device)
