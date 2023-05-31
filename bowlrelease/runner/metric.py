9326d20d586ac540d58c5d40f1c5d6d8aad38b7d
Davide Zambrano <davide.zambrano@gmail.com>
2023-05-31 11:40:16 +0200

    updating metrics

--- bowlrelease/runner/metric.py
+++ bowlrelease/runner/metric.py
@@ -14,23 +14,21 @@ IOU_THRESHOLD = 0.5
 def _compute_pq_sq_rq(det, ann):
     iou_sum, det_idxs_mth, ann_idxs_mth = _compute_matching(det, ann)
     fps = 0
-    for id_, _ in enumerate(det):
-        if id_ not in det_idxs_mth:
+    for id, _ in enumerate(det):
+        if id not in det_idxs_mth:
             fps += 1
     fns = 0
-    for id_, _ in enumerate(ann):
-        if id_ not in ann_idxs_mth:
+    for id, _ in enumerate(ann):
+        if id not in ann_idxs_mth:
             fns += 1
     tps = len(det_idxs_mth)
-    sq_ = iou_sum / tps
-    rq_ = tps / (tps + 0.5 * fns + 0.5 * fps)
-    pq_ = sq_ * rq_
 
-    return pq_, sq_, rq_
+    return tps, fps, fns, iou_sum
 
 
 def _compute_matching(det, ann):
     iou_matrix = iou(det[:, :4], ann[:, :4], np.zeros((len(ann)))).T
+    iou_matrix[iou_matrix < 0.5] = 0.0
     det_idxs, ann_idxs = linear_sum_assignment(iou_matrix.T, maximize=True)
     ann_idxs_mth, det_idxs_mth = [], []
     for anid, deid in zip(ann_idxs, det_idxs):
@@ -39,14 +37,14 @@ def _compute_matching(det, ann):
             det_idxs_mth.append(deid)
     ann_idxs_mth = np.array(ann_idxs_mth)
     det_idxs_mth = np.array(det_idxs_mth)
-    iou_sum = iou_matrix[iou_matrix > 0.5].sum()
+    iou_sum = iou_matrix.sum()
     return iou_sum, det_idxs_mth, ann_idxs_mth
 
 
 def compute_pq_metric(
     gt_data: Dict[str, Dict[str, List[int]]],
     pred_data: Dict[str, Dict[str, List[int]]],
-) -> Tuple[np.float64, np.float64, np.float64]:
+) -> Tuple[float, float, float]:
     """Panoptic Quality metric.
     It computes the mean of the Panoptic quality scores across all videos.
 
@@ -58,26 +56,25 @@ def compute_pq_metric(
     Returns:
         Tuple: Panoptic Quality, Segmentation Quality, Recognition Quality.
     """
-    msq = []
-    mrq = []
-    mpq = []
+    tps, fps, fns, iou_sum = 0.0, 0.0, 0.0, 0.0
     for video_key, video_val in gt_data.items():
         pred = pred_data.get(video_key, {})
         if not pred:
-            mpq.append(0)
-            msq.append(0)
-            mrq.append(0)
+            fns += len(video_val)
             continue
         det_video = [[d[0], 1, d[1], 1] for d in pred.values()]
         ann_video = [[d[0], 1, d[1], 1] for d in video_val.values()]
-        pq_, sq_, rq_ = _compute_pq_sq_rq(
+        tps_, fps_, fns_, iou_sum_ = _compute_pq_sq_rq(
             np.array(det_video), np.array(ann_video)
         )
-        mpq.append(pq_)
-        msq.append(sq_)
-        mrq.append(rq_)
-
-    return np.mean(mpq), np.mean(msq), np.mean(mrq)
+        tps += tps_
+        fps += fps_
+        fns += fns_
+        iou_sum += iou_sum_
+    sq_ = iou_sum / tps
+    rq_ = tps / (tps + 0.5 * fns + 0.5 * fps)
+    pq_ = sq_ * rq_
+    return pq_, sq_, rq_
 
 
 def iou_metric(gt, pred, weights, thre):
